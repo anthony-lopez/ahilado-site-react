5e9727c3214d43626c89a9aa25622799e45df14a
Anthony Lopez <Anthony.Lopez@student.csulb.edu>
2018-11-05 15:31:04 -0800

    Added a home page and a route to it

---
+++ ahilado-site/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
