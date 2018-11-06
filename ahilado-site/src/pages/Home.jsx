85cb1fb5f1d262bed804b530cbd9271c0433dd8d
Anthony Lopez <Anthony.Lopez@student.csulb.edu>
2018-11-05 17:40:04 -0800

    Initial implementation of homepage.

--- ahilado-site/src/pages/Home.jsx
+++ ahilado-site/src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import Logo from "../images/cubeLogo.svg";
+import Bay from "../images/bayBridge.jpg";
+import City from "../images/seattle.jpg";
 
 class Home extends Component {
   state = {};
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <img className="logo" src={Logo} alt="ahilado logo" />
+        <a href="#">
+          <img className="photo" src={Bay} />
+        </a>
+        <a href="#">
+          <img className="photo" src={City} />
+        </a>
       </div>
     );
   }
